040548b86342dd86cf3cff0028774e56ad506055
Alexander Mosquea <alexander.rob0@gmail.com>
2019-09-12 15:37:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,18 @@
 # require modules here
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  require 'yaml'
+  emoticons = YAML.load_file(file_path)
+  
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+
+  emoticons.keys.each{|i, n| 
+    puts result["get_meaning"][i] 
+  }
+  
 end
+
 
 def get_japanese_emoticon
   # code goes here
